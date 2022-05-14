f0173afa0d750d243f06c5d1891f1e06525c85a4
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2022-05-14 18:25:23 -0500

    change theme movies

--- app/src/main/java/com/nullpointer/moviescompose/ui/activitys/MainActivity.kt
+++ app/src/main/java/com/nullpointer/moviescompose/ui/activitys/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavGraph
 import androidx.navigation.compose.rememberNavController
 import com.nullpointer.moviescompose.presentation.CastViewModel
@@ -23,6 +24,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MoviesComposeTheme {
                 DestinationsNavHost(
